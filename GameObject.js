7d5d4107bea11a2ed3cc5a205ef18a541b1d1844
dumaki <93692498+dumaki@users.noreply.github.com>
2022-06-20 01:04:12 -0400

    Update GameObject.js

--- GameObject.js
+++ GameObject.js
@@ -7,7 +7,7 @@ class GameObject {
     this.direction = config.direction || "down";
     this.sprite = new Sprite({
       gameObject: this,
-      src: config.src || "images/characters/people/brett.png",
+      src: config.src || "/images/characters/people/brett.png",
     });
 
     this.behaviorLoop = config.behaviorLoop || [];
